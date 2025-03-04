1a91998bc9e2a7f1c84d17d6a214577337356e34
Nusk-Rbb <nuskrbb1741dn@gmail.com>
2025-03-04 17:24:06 +0900

    :sparkles: Add desktop apps, develop apps and tools

--- modules/packages/default.nix
+++ modules/packages/default.nix
@@ -8,7 +8,25 @@ in {
     options.modules.packages = { enable = mkEnableOption "packages"; };
     config = mkIf cfg.enable {
     	home.packages = with pkgs; [
-        zip unzip tree bc
+        # Desktop Apps
+        onlyoffice-bin
+        obsidian
+
+        # Develop Apps
+        vscode
+        discord
+        gh
+
+        # misc
+        file
+        bc
+        cowsay
+        texliveFull
+        fzf
+        zip
+        unzip
+        tree
+        bc
       ];
     };
 }
