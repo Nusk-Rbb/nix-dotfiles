b43189081b0c474ecb4242da3692a86e52b5541b
Nusk-Rbb <nuskrbb1741dn@gmail.com>
2025-03-05 10:04:20 +0900

    :sparkles: Modefied chrome enable

--- modules/google-chrome/default.nix
+++ modules/google-chrome/default.nix
@@ -6,5 +6,6 @@ let cfg = config.modules.google-chorme;
 in {
     options.modules.google-chorme = { enable = mkEnableOption "google-chrome"; };
     config = mkIf cfg.enable {
+        programs.google-chorme.enable = true;
     };
 }
